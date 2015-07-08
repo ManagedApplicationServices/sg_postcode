4bf758c1a8fcc7e387a5d9c365f91758566bdfbd
Duy Khoa <duykhoa12t@gmail.com>
2015-07-08 17:32:00 +0800

    feat: add #send_geo_request method for long_lat_converter

--- lib/sg_postcode/converters/long_lat_converter.rb
+++ lib/sg_postcode/converters/long_lat_converter.rb
@@ -4,10 +4,22 @@ module SgPostcode
     end
 
     def self.place_info(postcode)
-      send_geo_request(postcode, host: SgPostcode::Google)
+      send_geo_request(postcode, host: :Google)
     end
 
-    def self.send_geo_request(postcode, host: nil)
+    # Send request to host, and return the response
+    #
+    # @return
+    #
+    # @params
+    #   postcode
+    #   host: default is Google
+    #
+    # @example
+    # SgPostcode::LongLatConverter.send_geo_request("230000", host: :Google)
+    #
+    def self.send_geo_request(postcode, host: :Google)
+      return nil if Module.const_defined? host
     end
   end
 end
