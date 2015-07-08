9f28ee523563063564034ae431cfb18f26e2a8b6
Duy Khoa <duykhoa12t@gmail.com>
2015-07-08 11:12:03 +0800

    add #convert method for array class

--- lib/sg_postcode/array.rb
+++ lib/sg_postcode/array.rb
@@ -1,8 +1,21 @@
 module SgPostcode
   class Array
-    def initialize(postcodes, *opts)
+    def initialize(postcodes, opts = {})
       @postcodes = postcodes
       @opts = opts
+    end
+
+    # Convert to a long lat array
+    #
+    # @return [{ long: '123456', lat: '123456' }, ...]
+    #
+    # @params
+    #   error_ignore: ignore invalid postcode(s)
+    #
+    # @example
+    # SgPostcode::Array.new(postcodes).convert
+    #
+    def convert(error_ignore = true)
     end
   end
 end
