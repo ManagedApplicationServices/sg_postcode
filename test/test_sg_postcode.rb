1970da1323bd19c271509d445aff408f61ed2921
Duy Khoa <duykhoa12t@gmail.com>
2015-07-08 12:29:01 +0800

    rename test file to test_<file_name>

--- test/test_sg_postcode.rb
+++ test/test_sg_postcode.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
-class SgPostcodeTest < ActiveSupport::TestCase
+class TestSgPostcode
   def test_init_new_sg_postcode_array
     postcode_array = SgPostcode::Array.new([])
     assert_kind_of SgPostcode::Array, postcode_array
   end
 
-  def convert_to_long_lat_array
+  def test_convert_to_long_lat_array
     postcode_array = SgPostcode::Array.new([])
     postcode_array.convert
   end
