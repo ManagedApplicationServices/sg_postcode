4c86874426c69554fc347cc26783dcfb756fd98e
Duy Khoa <duykhoa12t@gmail.com>
2015-07-09 16:46:45 +0800

    add TODO method for Config

--- lib/sg_postcode/response/response_builder.rb
+++ lib/sg_postcode/response/response_builder.rb
@@ -6,6 +6,14 @@ module SgPostcode
           @fields ||= default_fields
         end
 
+        def add_key_path(key_name, *path)
+          #TODO
+        end
+
+        def remove_key_path(key_name)
+          #TODO
+        end
+
         def default_fields
           {
             format_address: ['formatted_address'],
