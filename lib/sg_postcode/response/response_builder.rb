d3073d6a475bf026e6fb0c52012808013947ae53
Duy Khoa <duykhoa12t@gmail.com>
2015-07-09 17:50:42 +0800

    feat: raise error for abstract method in response_builder class

--- lib/sg_postcode/response/response_builder.rb
+++ lib/sg_postcode/response/response_builder.rb
@@ -1,29 +1,5 @@
 module SgPostcode
   module ResponseBuilder
-    class Config
-      class << self
-        def fields
-          @fields ||= default_fields
-        end
-
-        def add_key_path(key_name, *path)
-          #TODO
-        end
-
-        def remove_key_path(key_name)
-          #TODO
-        end
-
-        def default_fields
-          {
-            format_address: ['formatted_address'],
-            lat: ['geometry', 'location', 'lat'],
-            long: ['geometry', 'location', 'lng'],
-          }
-        end
-      end
-    end
-
     class Builder
       def initialize(raw_data)
         @raw_data = raw_data['results'].try(:first)
@@ -34,19 +10,7 @@ module SgPostcode
       end
 
       def data
-        result = {}
-
-        Config.fields.each do |key, value|
-          result[key] = digg value
-        end
-
-        result
-      end
-
-      def digg(key_path)
-        key_path.inject(@raw_data) do |result, key|
-          (result.respond_to?(:[]) && (result[key] || {})) || break
-        end
+        raise NotImplementedError
       end
     end
   end
