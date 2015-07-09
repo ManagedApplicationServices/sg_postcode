56949a8113548ce6116d12c8651292f25e2e5343
Duy Khoa <duykhoa12t@gmail.com>
2015-07-09 11:16:54 +0800

    remove p command in test

--- test/converters/test_long_lat_converter.rb
+++ test/converters/test_long_lat_converter.rb
@@ -7,7 +7,6 @@ class TestLongLatConverter < Minitest::Test
 
   def test_send_geo_request
     result = SgPostcode::LongLatConverter.send_geo_request("23000", host: :NotExist)
-    p result
 
     refute_equal nil, result
   end
