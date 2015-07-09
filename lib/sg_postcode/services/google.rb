2082c6343ac5c53b9d2c8d3d0c33ad2543c22bc4
Duy Khoa <duykhoa12t@gmail.com>
2015-07-09 11:12:56 +0800

    send google map request

--- lib/sg_postcode/services/google.rb
+++ lib/sg_postcode/services/google.rb
@@ -1,10 +1,15 @@
+require 'rest-client'
+
 module SgPostcode
   class Google
+    REQUEST_SERVICE_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
+
     def initialize(postcode)
       @postcode = postcode
     end
 
     def request
+      RestClient.get REQUEST_SERVICE_URL, { params: { address: @postcode } }
     end
   end
 end
--- test/converters/test_long_lat_converter.rb
+++ test/converters/test_long_lat_converter.rb
@@ -7,7 +7,8 @@ class TestLongLatConverter < Minitest::Test
 
   def test_send_geo_request
     result = SgPostcode::LongLatConverter.send_geo_request("23000", host: :NotExist)
+    p result
 
-    assert_equal nil, result
+    refute_equal nil, result
   end
 end
