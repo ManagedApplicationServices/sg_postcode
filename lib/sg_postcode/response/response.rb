b8ad51f06a8181ee78d6db61ceaa6be4608dc154
Duy Khoa <duykhoa12t@gmail.com>
2015-07-09 17:49:40 +0800

    feat: return output as a son

--- lib/sg_postcode/response/response.rb
+++ lib/sg_postcode/response/response.rb
@@ -2,16 +2,21 @@ require 'json'
 
 module SgPostcode
   class Response
-    def initialize(raw_data)
+    def initialize(raw_data, response_type = :json)
       @raw_data = JSON.parse(raw_data)
+      @response_type = response_type
     end
 
     # build the data hash
     # may use Builder object to contruct the output data
     #
-    #
     def data
-      @raw_data['results'][0]['formatted_address']
+      case @response_type
+      when :json
+        ResponseBuilder::JsonOutput.new(@raw_data).data
+      else
+        nil
+      end
     end
   end
 end
