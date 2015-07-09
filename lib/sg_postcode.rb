5686004d86ac9f7a21f0c7e83cf473a517435205
Duy Khoa <duykhoa12t@gmail.com>
2015-07-09 17:51:04 +0800

    feat: require json_output and config class

--- lib/sg_postcode.rb
+++ lib/sg_postcode.rb
@@ -9,6 +9,8 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "sg_postcode", "response"))
 
 require "response"
 require "response_builder"
+require "json_output"
+require "config"
 require "array"
 require "google"
 require "long_lat_converter"
