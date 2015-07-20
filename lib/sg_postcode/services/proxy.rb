58a79e9a443adf05dbe5b94ebc8324b0a10229f7
Duy Khoa <duykhoa12t@gmail.com>
2015-07-20 14:53:22 +0800

    refactor: using attr_accessor in proxy class

--- lib/sg_postcode/services/proxy.rb
+++ lib/sg_postcode/services/proxy.rb
@@ -1,12 +1,15 @@
 module SgPostcode
   class Proxy
+    attr_reader :service
+    attr_reader :cache_engine
+
     def initialize(cache_engine, service)
       @cache_engine = cache_engine
       @service = service
     end
 
     def request
-      @service.request
+      service.request
     end
   end
 end
