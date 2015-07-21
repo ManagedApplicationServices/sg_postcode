57321a7af413aac263ba2ce4979076b2a5ca80b9
Duy Khoa <duykhoa12t@gmail.com>
2015-07-21 14:52:45 +0800

    demo: fix space

--- test/dummy/app/controllers/homes_controller.rb
+++ test/dummy/app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def index
-    postcode = params[:postcode].try :split, ','
+    postcode = params[:postcode].try :split, ', '
     result = SgPostcode::Array.new(postcode || []).convert
 
     @result = result.delete_if { |item| item[:format_address].nil? }
